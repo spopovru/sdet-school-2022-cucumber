8e2bdba86c17b02ff1d6fe43eb1558ab9b152f75
Sergey Popov <spopovru@gmail.com>
2022-06-24 00:28:45 +0300

    Change steps arguments to regexp

--- src/test/java/ru/nspk/calculator/test/steps/CalculatorSteps.java
+++ src/test/java/ru/nspk/calculator/test/steps/CalculatorSteps.java
@@ -12,47 +12,47 @@ public class CalculatorSteps {
 
     private Calculator calculator;
 
-    @Пусть("инициализирован новый калькулятор")
+    @Пусть("^инициализирован новый калькулятор$")
     public void initializeNewCalculator() {
         calculator = Calculator.create();
     }
 
-    @Пусть("первый аргумент равен {int}")
+    @Пусть("^первый аргумент равен (-?\\d+)")
     public void setArgument1(Integer arg) {
         calculator.setArgument1(arg);
     }
 
-    @Пусть("второй аргумент равен {int}")
+    @Пусть("^второй аргумент равен (-?\\d+)$")
     public void setArgument2(Integer arg) {
         calculator.setArgument2(arg);
     }
 
-    @Когда("выполнена операция сложения")
+    @Когда("^выполнена операция сложения$")
     public void executeAddition() {
         calculator.executeAddition();
     }
 
-    @Когда("выполнена операция вычитания")
+    @Когда("^выполнена операция вычитания$")
     public void executeSubtraction() {
         calculator.executeSubtraction();
     }
 
-    @Когда("выполнена операция умножения")
+    @Когда("^выполнена операция умножения$")
     public void executeMultiplication() {
         calculator.executeMultiplication();
     }
 
-    @Когда("выполнена операция деления")
+    @Когда("^выполнена операция деления$")
     public void executeDivision() {
         calculator.executeDivision();
     }
 
-    @Тогда("результат равен {int}")
+    @Тогда("^результат равен (-?\\d+)$")
     public void checkResult(Integer result) {
         assertEquals(result, calculator.getResult());
     }
 
-    @Тогда("операция деления приводит к ошибке")
+    @Тогда("^операция деления приводит к ошибке$")
     public void divisionThrowException() {
         assertThrows(Exception.class, () -> {
             calculator.executeDivision();
